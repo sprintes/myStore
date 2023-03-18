5a3454da8f807e321c747dca032b3043a26542e0
Sprint <sprint@pop-os.localdomain>
2023-03-18 19:31:46 +0700

    konfigurasi register

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,23 @@
+const bcrypt = require('bcryptjs');
 const {signup} = require('../validation/user');
+const salt = bcrypt.genSaltSync(10);
+
 const register = async (req, res) => {
     try {
-        const errMsg = register(req.body);
+        const errMsg = signup(req.body);
+        if(errMsg) return res.status(401).json(errMsg)
+        const passwordHashed = bcrypt.hashSync(req.body.password, salt);
+        const user = {
+            username: req.body.username,
+            email: req.body.email,
+            password: passwordHashed,
+            isAdmin: false,
+            refresh_token: '',
+        }
+        console.log(user);
         return res.status(200).json('Regster berhasil');
     }catch(err){
-        if(err) return res.status(500)
+        if(err) return res.status(500).json(err)
     }
 } 
 
